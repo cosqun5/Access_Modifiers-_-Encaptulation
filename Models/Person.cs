3910d9041bd38142b01c18d7359d1421230ae364
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-04-12 12:03:00 +0400

    first commit

---
+++ Models/Person.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers___Encaptulation.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            Console.WriteLine("Person Created");
+        }
+        public Person(string name, string surname, int age, int birthday)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+            this.birthday = birthday;
+        }
+
+        #region Encaptulation - u birinci fieldlərə get və set methodları
+        private string _name;
+        private int _age;
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+
+
+            set
+            {
+                if (value.Length >= 2)
+                {
+                    _name = value;
+                    return;
+                }
+                Console.WriteLine("{...the length of the name is small, 2 den ...}");
+            }
+        }
+        public string surname { get; set; }
+        public int age
+        {
+
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value >= 16)
+                {
+                    _age = value;
+                    return;
+                }
+                Console.WriteLine("{...your age is small...}");
+            }
+        }
+        public int birthday { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine("{");
+            Console.WriteLine("Name :" + $"{name}");
+            Console.WriteLine("Surname :" + $"{surname}");
+            Console.WriteLine("Age :" + $"{age}");
+            Console.WriteLine("Birthday :" + $"{birthday}");
+            Console.WriteLine("}");
+
+        }
+
+        #endregion
+
+
+        //#region propertinin get və set methodıarənə override edərək
+
+        //public Person(string name, string surname, int age, int birthday)
+        //{
+        //    _name = name;
+        //    this.surname = surname;
+        //    _age = age;
+        //    this.birthday = birthday;
+        //}
+
+        //private string _name;
+        //public string surname;
+        //private int _age;
+        //public int birthday;
+
+        //public void SetName(string value)
+        //{
+        //    if (value.Length >= 2)
+        //    {
+        //        _name = value;
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("{...the length of the name is small, 2 den ...}");
+
+        //    }
+        //}
+        //public string GetName()
+        //{
+
+        //    return _name;
+        //}
+        //public void SetAge(int value)
+        //{
+        //    if (value >= 16)
+        //    {
+        //        _age = value;
+
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("{...your age is small...}");
+
+        //    }
+        //}
+        //public int GetAge()
+        //{
+
+        //    return _age;
+        //}
+        //public void Print()
+        //{
+        //    Console.WriteLine("{");
+        //    Console.WriteLine("Name :" + $"{_name}");
+        //    Console.WriteLine("Surname :" + $"{surname}");
+        //    Console.WriteLine("Age :" + $"{_age}");
+        //    Console.WriteLine("Birthday :" + $"{birthday}");
+        //    Console.WriteLine("}");
+
+        //}
+        //#endregion
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Access_Modifiers___Encaptulation.Models;
+
+Console.Write("Enter name:");
+string name = Console.ReadLine();
+Console.Write("Enter surname:");
+string surname = Console.ReadLine();
+Console.Write("Enter age:");
+int age = int.Parse(Console.ReadLine());
+Console.Write("Enter birthday:");
+int birthday = int.Parse(Console.ReadLine());
+Person person = new Person(name,surname,age,birthday);
+
+
+//person.SetName(name);
+//string name1 =person.GetName();
+//person.SetAge(age);
+//string age1 = person.GetName();
+person.Print();
+
+
